02cf209e7ec94782e4c868e8a8736c5337ad508f
임선경 <kind4040@naver.com>
2023-03-24 17:58:43 +0900

    로그인 디자인2

--- dzfront/src/component/login/Login.jsx
+++ dzfront/src/component/login/Login.jsx
@@ -1,8 +1,7 @@
-import { Column } from "ag-grid-community";
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import '../../css/login.css';
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import "../../css/login.css";
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -18,7 +17,6 @@ function Login() {
   function handleSubmit(event) {
     event.preventDefault();
 
- 
     fetch("http://localhost:8080/login", {
       method: "POST",
       headers: {
@@ -31,64 +29,61 @@ function Login() {
     })
       .then((response) => response.json())
       .then((data) => {
-        if(data.isLogOn===true){
-
+        if (data.isLogOn === true) {
           Swal.fire({
-            title:data.member.worker_id+"님 로그인되었습니다.",
-            text:'환영합니다',
-            icon:'success',
-
+            title: data.member.worker_id + "님 로그인되었습니다.",
+            text: "환영합니다",
+            icon: "success",
           });
-         
+
           localStorage.setItem("worker_id", data.member.worker_id);
           localStorage.setItem("code_count", data.member.code_count);
           localStorage.setItem("isLogOn", 1);
           navigate("/");
-      
-      }
-        else{
+        } else {
           Swal.fire({
-            title:'로그인 실패',
-            text:'정보를 확인해주세요',
-            icon:'error',
-
+            title: "로그인 실패",
+            text: "정보를 확인해주세요",
+            icon: "error",
           });
-        
-
         }
       })
       .catch((error) => {
         console.error("Error:", error);
-        alert('실패');
+        alert("실패");
       });
   }
 
   return (
-  
-
-    <div className="box">
-    <form onSubmit={handleSubmit} style={{ flexDirection:"Column"}}>
-      <div className="content-container">
-    <div className="input-container">
-          <input type="text" placeholder="아이디"  value={username} onChange={handleUsernameChange}/>
-      
+    <div class="container">
+      <div class="display">
+        <div class="display_content">
+          <div class="login_form">
+            <div class="login_field">
+              <input
+                type="text"
+                class="login_input"
+                placeholder="아이디"
+              ></input>
+            </div>
+            <div class="login_field">
+              <input
+                type="password"
+                class="login_input"
+                placeholder="비밀번호"
+              ></input>
+            </div>
+            <button class="login_submit">로그인</button>
+          </div>
         </div>
-        
-        <div className="input-container">
-          <input type="password"  value={password} onChange={handlePasswordChange} placeholder="비밀번호"/>
-     
+        <div class="display_background">
+          <span class="display_background_shape display_background_shape4"></span>
+          <span class="display_background_shape display_background_shape3"></span>
+          <span class="display_background_shape display_background_shape2"></span>
+          <span class="display_background_shape display_background_shape1"></span>
         </div>
-        
-        <button type="submit">로그인</button>
-   </div>
-      </form>
       </div>
-   
- 
-   
-
- 
-    
+    </div>
   );
 }
 
