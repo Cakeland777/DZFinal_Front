c19e63403ae76d9fd5ed0b31b09fbcc881bb1e87
lmacrown <lmacrown@naver.com>
2023-03-27 17:09:48 +0900

    login & calendar update

--- dzfront/src/App.js
+++ dzfront/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useHistory, useNavigate  } from 'react-router-dom';
 import Login from './component/login/Login'
 import Model from './model/Model'
 import Register from './component/Register';
@@ -10,11 +10,25 @@ import Registration from './component/regist/Registration';
 import EarnerRead from './component/list/EarnerRead';
 import EarnDivRead from './component/list/EarnDivRead';
 import IncomeInput from './component/input/IncomeInput';
+import {useEffect} from 'react';
 import Test from './component/test';
 import Home from './component/Home';
 import Calender from './component/Calendar';
 
 function App() {
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   let status = localStorage.getItem("isLogOn");
+  //   if (status === "1") {
+  //     const timeoutId = setTimeout(() => {
+  //       localStorage.clear();
+  //     }, 500 * 60 * 1000); // 5분 = 5 * 60 * 1000 밀리초
+  //     return () => clearTimeout(timeoutId);
+  //   } else {
+  //     navigate('/login');
+  //   }
+  // }, [navigate]);
 
   return (
   <>
@@ -38,7 +52,8 @@ function App() {
   </>
   );
  
-};
+}
+
 
 export default App;
 
